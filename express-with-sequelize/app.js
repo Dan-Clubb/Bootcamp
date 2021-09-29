0ea8dc93c43ad872ac57c2a4bfef350ef87ac54b
Dan Clubb <danclubb18@gmail.com>
2021-09-29 09:51:40 +0100

    file of my work/learning during bootcamp

--- express-with-sequelize/app.js
+++ express-with-sequelize/app.js
@@ -9,16 +9,19 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// view all companies
 app.get("/companies", async (req, res) => {
     const companies = await Company.findAll();
     res.json(companies);
 });
 
+// veiw specific company
 app.get("/companies/:id", async (req, res) => {
     const companies = await Company.findByPk(req.params.id);
     res.json(companies);
 });
 
+// view all a company's menus
 app.get("/companies/:id/menus", async (req, res) => {
     const companies = await Menu.findAll({
         where: {
@@ -28,6 +31,7 @@ app.get("/companies/:id/menus", async (req, res) => {
     res.json(companies);
 });
 
+// create new company
 app.post("/companies", async (req, res) => {
     const newCompany = Company.create({
         name: req.body.name,
@@ -36,17 +40,20 @@ app.post("/companies", async (req, res) => {
         res.send("new company created");
 });
 
+// delete company
 app.delete("/companies/:id", async (req, res) => {
     const company = await Company.findByPk(req.params.id);
     company.destroy();
     res.send(`${company.name} deleted`);
 });
 
+// view specific menu
 app.get("/menus/:id", async (req, res) => {
     const menu = await Menu.findByPk(req.params.id);
     res.json(menu);
 });
 
+// replace specific company
 app.put("/companies/:id", async (req, res) => {
     const company = await Company.findByPk(req.params.id);
     if (!company) {
@@ -56,6 +63,7 @@ app.put("/companies/:id", async (req, res) => {
     res.send('company updated');
 });
 
+//
 app.post("companies/:id/menus", async (req, res) => {
     const company = req.params.id;
     await company.createMenu({
